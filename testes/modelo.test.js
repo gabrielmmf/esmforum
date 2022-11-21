69b350f8e916dae3745501d20feb83d34e4fa0aa
Gabriel Fialho <77760042+gabrielmmf@users.noreply.github.com>
2022-11-20 22:42:05 -0300

    100% Coverage tests

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,22 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
+  modelo.cadastrar_resposta(0,'2')
+  modelo.cadastrar_resposta(1,'4')
+  modelo.cadastrar_resposta(2,'6')
   const perguntas = modelo.listar_perguntas(); 
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  const respostas = modelo.get_respostas();
+  expect(respostas.length).toBe(0);
+  const pergunta0 = modelo.get_pergunta(0);
+  expect(pergunta0.id_pergunta).toBe(0);
+  expect(pergunta0.texto).toBe('1 + 1 = ?');
+
+  const resposta0 = modelo.get_pergunta[0];
+  expect(resposta0.id_pergunta).toBe(0);
+  expect(resposta0.texto).toBe('2');
 });
